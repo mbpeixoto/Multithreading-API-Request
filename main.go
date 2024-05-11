d2a6c0b89922aaef5765602a94d914513a4c9f46
Mateus Batista <batistamateus16@gmail.com>
2024-05-11 15:43:03 -0300

    Versão inicial

---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+type ViaCep struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
+
+type BrasilApi struct {
+	Cep          string `json:"cep"`
+	State        string `json:"state"`
+	City         string `json:"city"`
+	Neighborhood string `json:"neighborhood"`
+	Street       string `json:"street"`
+	Service      string `json:"service"`
+}
+
+func main() {
+	canal1 := make(chan BrasilApi)
+	canal2 := make(chan ViaCep)
+
+	cep := "71503503"
+
+	go func() {
+
+		req, err := http.Get("https://brasilapi.com.br/api/cep/v1/" + cep)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao fazer requisição: %v\n", err)
+		}
+		defer req.Body.Close()
+		res, err := io.ReadAll(req.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao ler resposta: %v\n", err)
+		}
+		var dados BrasilApi
+		err = json.Unmarshal(res, &dados)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao fazer parse da resposta: %v\n", err)
+		}
+		canal1 <- dados
+	}()
+
+	go func() {
+		req, err := http.Get("http://viacep.com.br/ws/" + cep + "/json/")
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao fazer requisição: %v\n", err)
+		}
+		defer req.Body.Close()
+		res, err := io.ReadAll(req.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao ler resposta: %v\n", err)
+		}
+		var dados ViaCep
+		err = json.Unmarshal(res, &dados)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao fazer parse da resposta: %v\n", err)
+		}
+		canal2 <- dados
+	}()
+
+	select {
+	case endereco := <-canal1:
+		fmt.Println("Api BrasilApi")
+		fmt.Printf("%+v\n", endereco)
+
+	case endereco := <-canal2:
+		fmt.Println("Api ViaCep")
+		fmt.Printf("%+v\n", endereco)
+
+	case <-time.After(1 * time.Second):
+		fmt.Println("Time out")
+	}
+
+}
